3a8eac031ecf75c1a2af1cd7174cc52469059217
lluuccaass88 <lucaspalves8@gmail.com>
2020-08-17 10:13:28 -0300

    commit

--- todolistApp/App.js
+++ todolistApp/App.js
@@ -4,19 +4,32 @@ import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList } from 'r
 import { Ionicons, MaterialIcons } from '@expo/vector-icons'; 
 
 export default function App() {
-  const [Task, setTask] = useState([])
+  const [task, setTask] = useState([])
   const [newTask, setNewTask] = useState('')
 
   return (
     <>
     <View style={styles.container}>
       <View style={styles.title}>
-        <FlatList style={styles.flatList} />
+        <FlatList
+        data={task}
+        keyExtractor={item => item.toString()}
+        showsVerticalScrollIndicator={false}
+        renderItem={({item}) => {
+          <View> 
+            <Text>{item}</Text>
+            <TouchableOpacity>
+              <MaterialIcons name="delete-forever" size={25} color="#f64c75" />
+            </TouchableOpacity>
+          </View>
+        }}
+        style={styles.flatList} 
+        />
       </View>
       <View style={styles.form}>
         <TextInput 
         style={styles.input} 
-        placeholderTextColor="#999"
+        placeholderTextColor="#f7fdff"
         autoCorrect={true}
         placeholder="Adicione uma tarefa"
         maxLength={25}
@@ -33,7 +46,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4e0091',
     paddingHorizontal: 20,
     paddingVertical: 20,
     marginTop: 20
@@ -54,19 +67,19 @@ const styles = StyleSheet.create({
   input: {
     flex: 1,
     height: 40,
-    backgroundColor: '#eee',
+    backgroundColor: '#006f91',
     borderRadius: 4,
     paddingVertical: 5,
     paddingHorizontal: 10,
     borderWidth: 1,
-    borderColor: '#eee'
+    borderColor: '#007da3'
   },
   button: {
     height: 40,
     width: 40,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#1c6cce',
+    backgroundColor: '#d47502',
     borderRadius: 4,
     marginLeft: 10
   },
